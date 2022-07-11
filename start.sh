a2ac75a53de0b9cfc5358f6f162989122c4fb70b
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-07-11 22:06:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/SECCOMPLEX/MariaFilter.git /MariaFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /MariaFilter
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
